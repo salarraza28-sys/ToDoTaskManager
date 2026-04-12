06ca2534276c6ff2def3658f5949a022ca273e4a
SalarRaza <salarraza28@gmail.com>
2026-04-12 18:17:59 +0530

    add functionality to create a todo

--- backend/routes/todo.route.js
+++ backend/routes/todo.route.js
@@ -9,4 +9,16 @@ router.get("/", async(req,res)=>{
         res.status(500).json({message: error.message})
     }
 })
+// Add a new todo
+router.post("/",async (req, res) =>{
+    const todo =new Todo({
+        text: req.body.text
+    })
+    try{
+        const newTodo = await todo.save();
+        res.status(201).json(newTodo)
+    } catch (error){
+        res.status(400).json({message: error.message})
+    }
+} )
 
